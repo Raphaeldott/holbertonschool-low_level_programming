e9bc373ebf2f731777cd857d198ba7b1d1725bf2
raphaeldott <raphael.dott@hotmail.com>
2024-10-21 22:58:50 +0200

    fastcommit

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10: should print 10 times
+ * _putchar: to add a new line
+ */
+void print_alphabet_x10(void)
+{
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	_putchar('\n');
+}
