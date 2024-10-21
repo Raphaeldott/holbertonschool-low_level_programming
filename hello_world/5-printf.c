d23a6e7a24a8592b283fab2d8049d0af0c5d5c94
raphaeldott <raphael.dott@hotmail.com>
2024-10-21 11:04:40 +0200

    bettys norm ok

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ * printf is printing the string
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+/* this function print a string */
