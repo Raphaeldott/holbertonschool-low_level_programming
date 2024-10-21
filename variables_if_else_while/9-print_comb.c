9f5fdf3bc48d0a05af0c59449c016645f2920edc
raphaeldott <raphael.dott@hotmail.com>
2024-10-21 17:01:01 +0200

    use % to ensure the one digit consign in all situation and used for instead while

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,14 +8,14 @@ int main(void)
 {
 	int i;
 
-	i = 0;
-	while (i <= 8)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar(i + '0');
+		putchar((i % 10) + '0');
+		if (i == 9)
+			continue;
 		putchar(',');
 		putchar(' ');
-		i++;
 	}
-	putchar('9');
+	putchar('\n');
 	return (0);
 }
