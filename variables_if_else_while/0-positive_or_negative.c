ce1924ca73ecaf7c68fbe321ae92be6f7979b709
raphaeldott <raphael.dott@hotmail.com>
2024-10-21 13:45:05 +0200

    removed argc, add srand generator

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 /**
  * main - entry of the number
  * Return: 0 (success)
- * Return1: 1 argc != 2
- * @argv: is used to input the parameter from the terminal
- * @argc: is the number of parameters
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-int n = atoi(argv[1]);
-if (argc != 2)
-printf("usage: %s <number> ", argv[0]);
-return (1);
-if (n > 0)
-printf("%d is positive\n", n);
-if (n < 0)
-printf("%d is negative\n", n);
-if (n == 0)
-printf("%d is zero\n", n);
-return (0);
+	int n = rand() - RAND_MAX / 2; /* Assign the number to n */
+
+	srand(time(NULL)); /* Initialize random number generator */
+	printf("%d\n", n);
+	if (n > 0)
+		printf("is positive\n");
+	if (n < 0)
+		printf("is negative\n");
+	if (n == 0)
+		printf("is zero\n");
+	return (0);
 }
 /* tell if the number is positive or negative */
