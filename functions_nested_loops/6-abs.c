3f192793d2881c30bd954dc99c7489eb538bef9d
raphaeldott <raphael.dott@hotmail.com>
2024-10-22 12:03:16 +0200

    fastcommit

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - return the absolute value
+ * @n: is the integer
+ * Return: -n if n is negative
+ * n if n is positive
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -7,4 +7,5 @@ void print_alphabet_x10(void);/*prototype 10 times alphabet */
 int _islower(int c);/* checks for lowercases */
 int _isalpha(int c);/* checks for letter */
 int print_sign(int n);/* check the sign */
+int _abs(int n);/* print the absolute value of an integer */
 #endif /* MAIN_H */
