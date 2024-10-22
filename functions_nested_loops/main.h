a745ac5342ec64d4aefd492d7c2be55c4aa5b68a
raphaeldott <raphael.dott@hotmail.com>
2024-10-22 11:47:05 +0200

    fastcommit

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,4 +6,5 @@ void print_alphabet(void);/* prototype for print_alphabet */
 void print_alphabet_x10(void);/*prototype 10 times alphabet */
 int _islower(int c);/* checks for lowercases */
 int _isalpha(int c);/* checks for letter */
+int print_sign(int n);/* check the sign */
 #endif /* MAIN_H */
