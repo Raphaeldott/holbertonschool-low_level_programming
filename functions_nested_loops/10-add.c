d5cf07c5c4f17153f52094c69e3559c451276dad
raphaeldott <raphael.dott@hotmail.com>
2024-10-22 15:14:34 +0200

    changed the whole code to reduce it to return (a + b)

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,21 +1,12 @@
 #include "main.h"
 /**
  *add - make an addition
- *Return: 0 (success)
  *@a: first number
  *@b: second number
+ *
+ * Return: the sum
  */
 int add(int a, int b)
 {
-	int result = a + b;
-
-	if (result < 10)
-		_putchar(result + '0');
-	else
-	{
-		_putchar((result / 10) + '0');
-		_putchar((result % 10) + '0');
-	}
-	_putchar('\n');
-	return (0);
+	return (a + b);
 }
