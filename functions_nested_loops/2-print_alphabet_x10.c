d0f3443536470e76d1da63fcdd1a7cfff30f2035
raphaeldott <raphael.dott@hotmail.com>
2024-10-22 10:32:30 +0200

    change the methode by using loops instead of calling TEN TIMES

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,18 +2,20 @@
 /**
  * print_alphabet_x10 - should print 10 times
  * _putchar: to add a new line
+ * alpha[]: takes a string
  */
 void print_alphabet_x10(void)
 {
-	print_alphabet();
-	print_alphabet();
-	print_alphabet();
-	print_alphabet();
-	print_alphabet();
-	print_alphabet();
-	print_alphabet();
-	print_alphabet();
-	print_alphabet();
-	print_alphabet();
-	_putchar('\n');
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			_putchar(alpha[j]);
+		}
+		_putchar('\n');
+	}
 }
