927cf24fa04d838eef3b74dc8f4c8689841fe318
raphaeldott <raphael.dott@hotmail.com>
2024-10-29 09:15:08 +0100

    fastcommit

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
- *
- *
+ *rev_string - reverse a string
+ *@s: string
  *
  */
 void rev_string(char *s)
 {
 	int length = 0;
 	int i;
+	char temp;
 
 	while (s[length] != '\0')
 		length++;
 
 	for (i = 0; i < length / 2; i++)
 	{
-		char temp = s[i];
+		temp = s[i];
 		s[i] = s[length - 1 - i];
 		s[length - 1 - i] = temp;
 	}
