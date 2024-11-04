718878586b74b59a700c359a8e1a66d1b35c6cc5
raphaeldott <raphael.dott@hotmail.com>
2024-11-04 15:22:49 +0100

    second com for brice

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,20 +1,16 @@
 #include "main.h"
 /**
- * _memset - fills memory with a constant byte.
- * @s: pointer to the memory area to be filled.
- * @b: the constant byte to fill the memory with.
- * @n: number of bytes to fill.
- *
- * Return: pointer to the memory area s.
+ *_memset - fills the first n bytes pointed par s et contiennent b
+ *@s: string
+ *@b: le byte que l'on veut fill
+ *@n: c'est le nombre de fois qu on veut ecrire bytes
+ *Return: s (string)
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
 		s[i] = b;
-	}
-
 	return (s);
 }
