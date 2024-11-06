e2b0316f32f6cf32bf14c07e0ef72d3259a25bea
raphaeldott <raphael.dott@hotmail.com>
2024-11-06 11:46:26 +0100

    betty

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -10,18 +10,5 @@ int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
-/*
-#include <stdio.h>
-
-int main(int argc, char *argv[])
-{
-if (argc < 2)
-{
-// Program is invoked without additional arguments, but still prints its name
-}
-printf("%s\n", argv[0]);
-return 0;
-}
-*/
