ec388506684e590efcf52769e0afc6950a2aedbc
raphaeldott <raphael.dott@hotmail.com>
2024-11-13 12:30:21 +0100

    add typedef to match the call name my_dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -10,11 +10,11 @@
  * Description: This struct holds basic information about a dog, including
  * its name, age, and owner.
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} my_dog;
 
 #endif /* DOG_H */
